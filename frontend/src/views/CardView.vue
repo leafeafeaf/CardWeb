89c66c92167b1cf2451c53c7b926de52c8fc2193
김성민 <rlatjdals369@naver.com>
2024-03-26 18:00:44 +0900

    카드페이지 임시구현

---
+++ frontend/src/views/CardView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>카드 페이지 입니다.</div>
+  <div class="card" @mousemove="rotateCard"></div>
+  <div class="card" @mousemove="rotateCard"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+</template>
+<script>
+export default {
+  name: "CardView",
+  data() {
+    return {};
+  },
+  methods: {
+    rotateCard($event) {
+      console.log($event);
+    },
+  },
+};
+</script>
+<style scoped>
+.card {
+  background-image: url("../assets/card1.png");
+  width: 18rem;
+  height: 25rem;
+  background-size: cover;
+}
+</style>
